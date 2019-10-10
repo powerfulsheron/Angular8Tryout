410b9282256904c73affb9d70d5ba4c910b7c9f6
powerfulsheron <lorenzo.canavaggio@laposte.net>
2019-10-11 01:24:10 +0200

    feature(detail):Functionnal user detail :sparkles:

---
+++ src/app/user-detail/user-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../users/user';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
+})
+export class UserDetailComponent implements OnInit {
+
+  @Input() selectedUser: User;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
